6cbe9da073758b222eda41c25b2172c6c800676e
Benjamin Valin <chbench1502@gmail.com>
2020-11-04 09:50:08 +0100

    Ajout rôle secrétaire

---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Secretaire.java
@@ -0,0 +1,11 @@
+package be.technifutur.java2020.gestionstage;
+
+public class Secretaire implements Gestionnaires {
+
+    private String name = "Secrétaire";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
