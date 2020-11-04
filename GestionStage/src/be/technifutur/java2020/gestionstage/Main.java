5bdbad8ea05a2d66bf1ae38c7060401c82285cfd
Benjamin Valin <chbench1502@gmail.com>
2020-11-04 09:22:12 +0100

    Création classe Main

---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Main.java
@@ -0,0 +1,8 @@
+package be.technifutur.java2020.gestionstage;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue dans le gestionnaire de stages");
+        
+    }
+}
