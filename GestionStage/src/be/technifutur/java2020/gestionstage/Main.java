9337606c5b128e1e542ddb13be8f76032519e93d
Benjamin Valin <chbench1502@gmail.com>
2020-11-04 09:24:44 +0100

    Création Menu Gestionnaires + relation avec Main

--- GestionStage/src/be/technifutur/java2020/gestionstage/Main.java
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Main.java
@@ -2,7 +2,10 @@ package be.technifutur.java2020.gestionstage;
 
 public class Main {
     public static void main(String[] args) {
+
+        MenuGestionnaires menu = new MenuGestionnaires();
         System.out.println("Bienvenue dans le gestionnaire de stages");
-        
+        menu.start();
+
     }
 }
---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/MenuGestionnaires.java
@@ -0,0 +1,8 @@
+package be.technifutur.java2020.gestionstage;
+
+public class MenuGestionnaires {
+
+    public void start(){
+        
+    }
+}
