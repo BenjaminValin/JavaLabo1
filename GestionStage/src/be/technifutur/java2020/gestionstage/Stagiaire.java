fe3827d480e3d7a71c75282412a7c2ad0de40083
Benjamin Valin <chbench1502@gmail.com>
2020-11-04 09:50:44 +0100

    Création rôle stagiaire

---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Stagiaire.java
@@ -0,0 +1,11 @@
+package be.technifutur.java2020.gestionstage;
+
+public class Stagiaire implements Gestionnaires {
+
+    private String name = "Stagiaire";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
