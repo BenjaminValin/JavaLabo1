4022f6929c1b0b9c59f5ab88305f7773859ce0f8
Benjamin Valin <chbench1502@gmail.com>
2020-11-04 09:34:36 +0100

    Création organisateur

---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Organisateur.java
@@ -0,0 +1,11 @@
+package be.technifutur.java2020.gestionstage;
+
+public class Organisateur implements Gestionnaires {
+
+    private String name = "Organisateur";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
