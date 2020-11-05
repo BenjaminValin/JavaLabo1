3589ad734d26c5657f39e519bc91efe9d451927d
Benjamin Valin <chbench1502@gmail.com>
2020-11-05 13:30:33 +0100

    Ajout classe de test
    Classe Test permet d'insérer automatiquement du texte venant d'un fichier

---
+++ GestionStage/src/be/technifutur/java2020/gestionstage/Test.java
@@ -0,0 +1,35 @@
+package be.technifutur.java2020.gestionstage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Test {
+
+    public static int ligne = 0;
+    public static int derniereligne = 28;
+
+    public static String Texte() throws FileNotFoundException {
+        File texte = new File("text.txt");
+        Scanner scanner = new Scanner(texte);
+        String stock = "";
+        if (ligne <= derniereligne){
+            int l = 0;
+            while (l<=ligne){
+                stock = scanner.nextLine();
+                l++;
+            }
+        }
+        ligne++;
+        return stock;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(Texte());
+        System.out.println(Texte());
+        System.out.println(Texte());
+        System.out.println(Texte());
+        System.out.println(Texte());
+        System.out.println(Texte());
+    }
+}
